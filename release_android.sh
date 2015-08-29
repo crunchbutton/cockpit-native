dab83622d3cc35601d0d6fc70d34c126ff1b5056
Devin Smith <github@arzynik.com>
2015-08-29 10:37:10 -0700

    change to new android path

--- release_android.sh
+++ release_android.sh
@@ -4,7 +4,7 @@ rm -f platforms/android/ant-build/Cockpit.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
 -keystore cert/android/com.crunchbutton.cockpit.keystore \
 -storepass ***REMOVED*** \
-platforms/android/ant-build/CordovaApp-release-unsigned.apk \
+platforms/android/build/outputs/apk/android-release-unsigned.apk \
 com.crunchbutton.cockpit
-~/android/sdk/build-tools/19.1.0/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/Cockpit.apk
-open platforms/android/ant-build/
+~/android/sdk/build-tools/19.1.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/Cockpit.apk
+open platforms/android/build/outputs/apk/
